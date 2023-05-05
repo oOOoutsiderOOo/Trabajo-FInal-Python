2235b96a56e785879ffaf4b15f7c6fda37e7afb8
Nicolás Ciancaglini <trancesoup@gmail.com>
2023-05-04 22:10:39 -0300

    Creación modelo comment y migración a la bd

--- blog/comments/models.py
+++ blog/comments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    post_id = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
+    response_to_id = models.ForeignKey('self', on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted = models.BooleanField(default=False)
+    is_response = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.content
