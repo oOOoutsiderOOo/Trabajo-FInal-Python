a872b3bd6c3bd2a0b94b9a3aae82ca3d9c32261e
Nicolás Ciancaglini <trancesoup@gmail.com>
2023-05-08 01:46:08 -0300

    agregada carpeta media, campo image en Post. Configurado para usar imagenes

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +30,5 @@ urlpatterns = [
     path('', include('comments.urls')),
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
